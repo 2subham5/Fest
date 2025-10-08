c9666be0773294a67c94cd74d93f8124581ed8d9
Subham Kumar Sharma <subhamrelax45@gmail.com>
2025-10-08 13:53:11 +0530

    add build

--- backend/index.js
+++ backend/index.js
@@ -4,11 +4,19 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const userRouter = require('./routes/user.js');
 const paymentRouter = require('./routes/payment.js');
-
+const path = require("path");
 const app = express();
 dotenv.config();
 app.use(cors())
 app.use(express.json());
+
+app.use('/user',userRouter);
+app.use('/payment',paymentRouter);
+
+app.use(express.static(path.join(__dirname, "public")));
+app.get(/.*/, (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
 mongoose.set('strictQuery', false);
 
@@ -24,8 +32,6 @@ mongoose.connect(process.env.MONGODB_URL, {
     console.error("❌ MongoDB connection error:", err);
 });
 
-app.use('/user',userRouter);
-app.use('/payment',paymentRouter);
 
 app.listen(3000, (req,res)=>{
     console.log("App listen at port 3000");
