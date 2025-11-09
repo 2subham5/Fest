819c17aba11b68be7b307ac3cba13016497be77e
Subham Kumar Sharma <subhamrelax45@gmail.com>
2025-11-09 12:45:29 +0530

    add-phonenumber

--- frontend/src/Components/EventPaymentTable.jsx
+++ frontend/src/Components/EventPaymentTable.jsx
@@ -45,6 +45,7 @@ const EventPaymentTable = () => {
   });
 
   const columns = [
+    { name: "Phone Number", selector: row => row.mobileNumber || "-", sortable: true },
     { name: "Individual Name", selector: row => row.individualName || "-", sortable: true },
     { name: "Leader Name", selector: row => row.leaderName || "-", sortable: true },
     { name: "Team Name", selector: row => row.teamName || "-", sortable: true },
